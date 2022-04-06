fb3b9b62d2cd69e904f051492e330697fd585ce1
atticus-lv <1029910278@qq.com>
2022-04-06 22:42:54 +0800

    toggle_lightgroup_visibility/solo_lightgroup_object op

--- op_set_active_obj.py
+++ op_set_active_obj.py
@@ -40,10 +40,61 @@ class VIEW_OT_select_obj_by_lightgroup(bpy.types.Operator):
         return {'FINISHED'}
 
 
+# set lightgroup object visibility
+class VIEW_OT_toggle_lightgroup_visibility(bpy.types.Operator):
+    """ Toggle lightgroup object visibility """
+    bl_idname = "view.toggle_lightgroup_visibility"
+    bl_label = "Toggle Lightgroup Visibility"
+    bl_options = {'INTERNAL', 'UNDO'}
+
+    lightgroup: bpy.props.StringProperty(name="Lightgroup")
+
+    def invoke(self, context, event):
+        from .ui import get_obj_list_in_lightgroup
+        fit_list = get_obj_list_in_lightgroup(self.lightgroup)
+        vis = not fit_list[0].hide_viewport
+        
+        for obj in fit_list:
+            obj.hide_viewport = vis
+            obj.hide_render = vis
+
+        return {'FINISHED'}
+
+
+# solo lightgroup object
+class VIEW_OT_solo_lightgroup_object(bpy.types.Operator):
+    bl_idname = "view.solo_lightgroup_object"
+    bl_label = "Solo Lightgroup Object"
+    bl_option = {'INTERNAL', 'UNDO'}
+
+    lightgroup: bpy.props.StringProperty(name="Lightgroup")
+
+    def execute(self, context):
+        # get all lightgroup objects
+        from .ui import get_obj_list_in_lightgroup
+        for lightgroup_item in context.view_layer.lightgroups:
+            obj_list = get_obj_list_in_lightgroup(lightgroup_item.name)
+            if lightgroup_item.name != self.lightgroup:
+                for obj in obj_list:
+                    obj.hide_viewport = True
+                    obj.hide_render = True
+            else:
+                for obj in obj_list:
+                    obj.hide_viewport = False
+                    obj.hide_render = False
+
+        return {'FINISHED'}
+
+
 def register():
     bpy.utils.register_class(VIEW_OT_set_active_obj)
     bpy.utils.register_class(VIEW_OT_select_obj_by_lightgroup)
+    bpy.utils.register_class(VIEW_OT_toggle_lightgroup_visibility)
+    bpy.utils.register_class(VIEW_OT_solo_lightgroup_object)
+
 
 def unregister():
     bpy.utils.unregister_class(VIEW_OT_set_active_obj)
     bpy.utils.unregister_class(VIEW_OT_select_obj_by_lightgroup)
+    bpy.utils.unregister_class(VIEW_OT_toggle_lightgroup_visibility)
+    bpy.utils.unregister_class(VIEW_OT_solo_lightgroup_object)
