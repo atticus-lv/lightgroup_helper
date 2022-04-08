d3db2398282b4c0cdcbc9a14172be89ae2494785
atticus-lv <1029910278@qq.com>
2022-04-09 02:00:56 +0800

    bump version to 0.4

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Light Group Helper",
     "author": "Atticus",
     "blender": (3, 2, 0),
-    "version": (0, 3),
+    "version": (0, 4),
     "category": "Lighting",
     "support": "COMMUNITY",
     "doc_url": "",
